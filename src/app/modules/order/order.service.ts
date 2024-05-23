f38d93d55a41132d97ef396eae25f6605dff3cf2
Rakib <khandokerrakib.hasan@bracits.com>
2024-05-23 22:01:31 +0600

    feat(order): check product availability and stock update process implement

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,9 +1,42 @@
+import { Product } from "../product/product.model";
 import { TOrder } from "./order.interface";
 import { Order } from "./order.model";
 
 const createOrder = async (orderData: TOrder) => {
-    const result = await Order.create(orderData);
-    return result;
+    const session = await Order.startSession();
+    session.startTransaction();
+    try {
+        //first find the product
+        const product = await Product.findById(orderData.productId).session(
+            session
+        );
+
+        //checking whether the product is available or not
+        if (!product) {
+            throw new Error("Product not found");
+        }
+
+        //checking the stock
+        if (product.inventory.quantity < orderData.quantity) {
+            throw new Error("Not enough stock available");
+        }
+
+        //updating the quantity
+        product.inventory.quantity -= orderData.quantity;
+        product.inventory.inStock = product.inventory.quantity > 0;
+        await product.save({ session });
+
+        const order = new Order(orderData);
+        await order.save({ session });
+
+        await session.commitTransaction();
+        return order;
+    } catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        session.endSession();
+    }
 };
 
 const getAllOrders = async (email: string) => {
